303fdd57b58e67db40f717d84eaf55edfb45657e
Sergii Butenko <sergii.butenko@globallogic.com>
2024-04-29 09:18:35 +0200

    first commit with IDE

--- test_login.py
+++ test_login.py
@@ -5,6 +5,55 @@ def test_login_positive():
     # check message
 
 
+def test_login_anegative2():
+    pass
+
+
+def test_login_negative():
+    pass
+    # open pgae
+    # enter creds
+    # check messagedef test_login_positive():
+    pass
+    # open pgae
+    # enter creds
+    # check message
+
+
+def test_login_anegative2():
+    pass
+
+
+def test_login_negative():
+    pass
+    # open pgae
+    # enter creds
+    # check messagedef test_login_positive():
+    pass
+    # open pgae
+    # enter creds
+    # check message
+
+
+def test_login_anegative2():
+    pass
+
+
+def test_login_negative():
+    pass
+    # open pgae
+    # enter creds
+    # check messagedef test_login_positive():
+    pass
+    # open pgae
+    # enter creds
+    # check message
+
+
+def test_login_anegative2():
+    pass
+
+
 def test_login_negative():
     pass
     # open pgae
